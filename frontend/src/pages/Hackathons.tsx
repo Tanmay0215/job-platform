da55fb882bc7422c1e15c42ab3cbfcc1975f52e1
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-04-17 16:30:10 +0530

    ui changes

--- frontend/src/pages/Hackathons.tsx
+++ frontend/src/pages/Hackathons.tsx
@@ -1,39 +1,60 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Hackathon } from '../types/type';
+import { ArrowUpRight } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 const Internships = () => {
+  const navigate = useNavigate()
   const [data, setData] = useState<Hackathon>();
+  const [loading, setLoading] = useState(true);
+  
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+      const response = await axios.get(`${BACKEND_URL}/api/hackathons`);
+      setData(response.data);
+    } catch (error: any) {
+      console.error('Error fetching data:', error);
+      alert(error.message)
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
-        const response = await axios.get(`${BACKEND_URL}/api/hackathons`);
-        setData(response.data);
-      } catch (error: any) {
-        console.error('Error fetching data:', error);
-        alert(error.message)
-      }
-    };
     fetchData();
   }
     , []);
 
+  if (loading) {
+    return (
+      <div className='min-h-screen flex justify-center items-center'>
+        <h1 className='text-4xl'>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6">
       <div className="max-w-7xl mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {data && data.map((item, index) => (
-              <div key={index} className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition duration-200">
-                <p className="text-lg font-bold text-gray-900 mb-1">{item.companyName}</p>
-                <h2 className="text-md font-semibold text-blue-600">Eligibility: {item.eligibility}</h2>
-                <h2 className="text-md font-semibold text-green-600">Location: {item.location}</h2>
-                <p className="text-gray-700">{item.role}</p>
-                <a href={item.applicationLink}>Apply Here</a>
-              </div>
-            ))}
-          </div>
+       <h1 className='text-center text-3xl md:text-4xl font-medium p-5'>HACKATHONS</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {data && data.map((item, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-md p-6 hover:shadow-lg transition duration-200">
+              <p className="text-lg font-bold text-gray-900 mb-1">{item.companyName}</p>
+              <h2 className="text-md font-semibold text-blue-600">Eligibility: {item.eligibility}</h2>
+              <h2 className="text-md font-semibold text-green-600">Location: {item.location}</h2>
+              <p className="text-gray-700">{item.role}</p>
+              <button
+               onClick={() => navigate(item.applicationLink)}
+               className='flex items-center text-blue-500 hover:text-blue-700 font-semibold mt-2'>
+                Apply Here
+                <ArrowUpRight />
+                </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
--- frontend/src/pages/Opportunities.tsx
+++ frontend/src/pages/Opportunities.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Opportunities = () => {
   const navigate = useNavigate()
   return (
-    <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-3xl mx-auto">
-        <h1 className="text-3xl font-extrabold text-gray-900">Explore Opportunities</h1>
-        <p className="mt-4 text-gray-500">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-3xl mx-auto space-y-8">
+        <h1 className="text-5xl font-extrabold text-gray-900">Explore Opportunities</h1>
+        <p className="text-xl text-gray-500">
           Find the perfect opportunity to grow your career. Browse through hackathons, jobs, and internships.
         </p>
-        <div className="mt-6 flex space-x-4">
+        <div className="mt-6 flex gap-4">
           <button
             onClick={() => navigate('/hackathons')}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
